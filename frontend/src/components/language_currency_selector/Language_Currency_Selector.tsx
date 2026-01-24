98b82c033f56e51978ed9f802279ecdc1a704df0
Jacob <kisielius.jok@gmail.com>
2026-01-24 13:32:03 +0200

    grid fix

--- frontend/src/components/language_currency_selector/Language_Currency_Selector.tsx
+++ frontend/src/components/language_currency_selector/Language_Currency_Selector.tsx
@@ -2,7 +2,7 @@ import Lithuania from '../../assets/lithuania.svg';
 
 export default function Language_Currency_Selector() {
     return (
-        <div className="md:flex items-center lg:gap-2 gap-1 w-full hidden">
+        <div className="lg:flex items-center lg:gap-2 gap-1 w-full hidden">
             <img src={Lithuania} alt="Lithuania" className="h-4 w-4" />
             <p className="text-white lg:text-sm text-xs font-light">English EU</p>
             <p className="text-white lg:text-sm text-xs font-light">|</p>
